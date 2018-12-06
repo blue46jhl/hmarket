8c00abbed9f5cb99fbe8496ce1bc876e9b205fa5
James Lee <blue46jhl@gmail.com>
2018-12-06 16:01:36 -0500

    changed tables in app

--- application.py
+++ application.py
@@ -76,10 +76,10 @@ Session(app)
 @app.route("/")
 def index():
     if (session.get("user_id") is not None):
-        items =db.execute("SELECT * FROM item")
+        items =db.execute("SELECT * FROM "item"")
         return render_template("index.html", items=items)
     else:
-        items=db.execute("SELECT * FROM item")
+        items=db.execute("SELECT * FROM "item"")
         return render_template("homepage.html", items=items)
 
 
@@ -87,7 +87,7 @@ def index():
 def register():
     if request.method == "POST":
 
-        result = db.execute("SELECT * FROM user WHERE username = :username",
+        result = db.execute("SELECT * FROM "user" WHERE username = :username",
 
                             username=request.form.get("username"))
         print("check")
@@ -115,7 +115,7 @@ def register():
         if not password == confirm_password:
             flash("Passwords are not identical!")
             return render_template("register.html")
-        db.execute("INSERT INTO user(username, hash,status) VALUES (:username, :hash, :status)", username=request.form.get("username"),hash=generate_password_hash(request.form.get("password")), status=0)
+        db.execute("INSERT INTO "user"(username, hash,status) VALUES (:username, :hash, :status)", username=request.form.get("username"),hash=generate_password_hash(request.form.get("password")), status=0)
         token=encrypt(username)
         email=token
         print(token)
@@ -166,7 +166,7 @@ def sell():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         reference_url=(url_for('static',filename=filename))
         price = '${:,.2f}'.format(float(request.form.get("price")))
-        db.execute("INSERT INTO item(category, title, description, price, image,notes, seller_id) VALUES (:category, :title, :description, :price, :image, :notes, :seller_id)",
+        db.execute("INSERT INTO "item"(category, title, description, price, image,notes, seller_id) VALUES (:category, :title, :description, :price, :image, :notes, :seller_id)",
                         category= request.form.get("category"), title=request.form.get("title"),
                         description= request.form.get("description"), price=price, image = reference_url, notes=request.form.get("notes"), seller_id=session["user_id"])
         # return redirect ("/", invalidImage=invalidImage, init=True)
@@ -181,7 +181,7 @@ def buy(u_id):
         print("check")
         u_id=request.view_args["u_id"]
         buyer_email = session["user_id"]
-        seller_email_dictionary = db.execute("SELECT seller_id FROM item WHERE id=:u_id", u_id=u_id)
+        seller_email_dictionary = db.execute("SELECT seller_id FROM "item" WHERE id=:u_id", u_id=u_id)
         print(u_id)
         print(buyer_email)
         seller_email=seller_email_dictionary[0]["seller_id"]
@@ -192,24 +192,24 @@ def buy(u_id):
         mail.send(msg)
         return render_template("sold.html")
     else:
-        item=db.execute("SELECT * FROM item where id=:u_id", u_id=u_id)
+        item=db.execute("SELECT * FROM "item" where id=:u_id", u_id=u_id)
         u_id=u_id
         return render_template("buy.html", item=item)
 
 @app.route("/myitems", methods=["GET", "POST"])
 def myitems():
     if request.method=="POST":
-        item=db.execute("SELECT * FROM item where seller_id=:user", user=session["user_id"])
+        item=db.execute("SELECT * FROM "item" where seller_id=:user", user=session["user_id"])
         print(item)
         selected=request.form.getlist('item')
         for value in selected:
-            db.execute("DELETE from item where id=:u_id",u_id=value)
+            db.execute("DELETE from "item" where id=:u_id",u_id=value)
         # for item in item:
         #     if request.form.get('item'):
         #         db.execute("DELETE from item where id=:u_id", u_id=item["id"])
         return redirect("/")
     else:
-        item=db.execute("SELECT * FROM item where seller_id=:user", user=session["user_id"])
+        item=db.execute("SELECT * FROM "item" where seller_id=:user", user=session["user_id"])
         return render_template("myitems.html", item=item)
 
 # @app.route("/cart", methods =["GET", "POST"])
@@ -227,14 +227,14 @@ def confirm_email(token):
     except:
         redirect(url_for('unconfirmed'))
     print(email)
-    user = db.execute("SELECT * FROM user WHERE username = :email", email=email)
+    user = db.execute("SELECT * FROM "user" WHERE username = :email", email=email)
     print(user)
     #if (user.status=True):
         #print("already confirmed!")
     #else:
         #user.status= True
     user[0]["status"] = 1
-    db.execute("UPDATE user SET status = :new_status", new_status = user[0]["status"])
+    db.execute("UPDATE "user" SET status = :new_status", new_status = user[0]["status"])
     return redirect(url_for("login"))
 
 @app.route('/unconfirmed')
@@ -280,7 +280,7 @@ def login():
             return render_template("login.html")
 
         # Query database for username
-        rows = db.execute("SELECT * FROM user WHERE username = :username",
+        rows = db.execute("SELECT * FROM "user" WHERE username = :username",
                           username=request.form.get("username"))
 
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
@@ -316,33 +316,33 @@ def terms():
 
 @app.route("/books")
 def books():
-    items=db.execute("SELECT * from item WHERE category=:category", category="books")
+    items=db.execute("SELECT * from "item" WHERE category=:category", category="books")
     return render_template("index.html", items=items)
 
 @app.route("/other")
 def other():
-    items=db.execute("SELECT * FROM item WHERE category=:category", category="other")
+    items=db.execute("SELECT * FROM "item" WHERE category=:category", category="other")
     return render_template("index.html",items=items)
 
 
 @app.route("/technology")
 def technology():
-    items=db.execute("SELECT * FROM item WHERE category=:category", category="technology")
+    items=db.execute("SELECT * FROM "item" WHERE category=:category", category="technology")
     return render_template("index.html", items=items)
 
 @app.route("/furniture")
 def furniture():
-    items=db.execute("SELECT * FROM item WHERE category=:category", category="furniture")
+    items=db.execute("SELECT * FROM "item" WHERE category=:category", category="furniture")
     return render_template("index.html", items=items)
 
 @app.route("/tickets")
 def tickets():
-    items=db.execute("SELECT * FROM item WHERE category=:category", category="tickets")
+    items=db.execute("SELECT * FROM "item" WHERE category=:category", category="tickets")
     return render_template("index.html", items=items)
 
 @app.route("/apparel")
 def apparel():
-    items = db.execute("SELECT * FROM item WHERE category=:category", category="apparel")
+    items = db.execute("SELECT * FROM "item" WHERE category=:category", category="apparel")
     return render_template("index.html", items = items)
 
 if __name__ == '__main__':
